5533140cc35618dbfe3c7551e8974d16e3d3afda
KgrnDeveloper <kgrndeveloper@gmail.com>
2024-09-23 16:55:51 +0530

    Updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,9 +4,9 @@ import Footer from "@/components/H & F/Footer";
 
 export const metadata = {
   title: {
-    absolute : '',
-    default : "KGRN Shipping Services",
-    template : "%s - KGRN"
+    absolute: "",
+    default: "KGRN Shipping Services",
+    template: "%s - KGRN",
   },
   description: "KGRN Shipping",
 };
@@ -14,6 +14,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4S69S1F8JC"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4S69S1F8JC', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className="relative">
         <Header />
         <main>{children}</main>
